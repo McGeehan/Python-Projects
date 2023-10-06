74c5c5aa77b0241e9ce0a5a0f581ed6608716616
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-06 14:39:46 -0400

    Add files via upload

---
+++ product.py
@@ -0,0 +1,10 @@
+#calculate the product of 3 numbers
+
+#get some user input
+num1 = float(input("Enter first number: ")) #create a variable, accept input of type float.... a float is a decimal number like 1.2
+num2 = float(input("Enter second number: ")) #create second variable
+num3 = float(input("Enter third number: "))#accept third variable
+
+product = num1 * num2 * num3 #accept the input of numbers and multiply them together
+
+print("The product is: " + str(product)) #concatenation - combining variables together
